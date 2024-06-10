ad4d1f8bcd47d6deffd8063ae7befcd986bb09fb
Sumit Parya <sumitparya2004@gmail.com>
2024-06-11 02:22:26 +0530

    pass stage 1

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
+    sys.stdout.write("$ ")
+    sys.stdout.flush()
 
     # Wait for user input
     input()
