bc683c3581746afe3980afcfada591d2b4549e87
Sumit Parya <sumitparya2004@gmail.com>
2024-06-11 02:37:34 +0530

    stage 3

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def main():
         sys.stdout.flush()
         # Wait for user input
         command = input()
-        sys.stdout.write(f"{command}: command not found")
+        sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
