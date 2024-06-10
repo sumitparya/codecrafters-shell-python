e562c831f31f245a625871fd9f5c198cc7f9156d
Sumit Parya <sumitparya2004@gmail.com>
2024-06-11 03:20:26 +0530

    stage 6

--- app/main.py
+++ app/main.py
@@ -10,20 +10,18 @@ def main():
         command = input()
         cmd = command.split(" ",1)[0]
         if cmd not in commands_list:
-            sys.stdout.write(f"{command}: command not found\n")
-            sys.stdout.flush()
+            sys.stdout.write(f"{command}: not found\n")
         else:
             if cmd == "exit":
                 sys.exit(0)
             elif cmd == "echo":
                 sys.stdout.write(command.split(" ",1)[1] + "\n")
-                sys.stdout.flush()
             elif cmd == "type":
                 if command.split(" ",1)[1] in commands_list:
                     sys.stdout.write(f"{command.split(' ',1)[1]} is a shell builtin\n")
                 else:
-                    sys.stdout.write(f"{command.split(' ',1)[1]}: command not found\n")
-                sys.stdout.flush()
+                    sys.stdout.write(f"{command.split(' ',1)[1]}: not found\n")
+        sys.stdout.flush()
         
 
 if __name__ == "__main__":
