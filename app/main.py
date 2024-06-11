96c6a45efeb8a5448fb9ecfe4fba6f1a0ce732be
Sumit Parya <sumitparya2004@gmail.com>
2024-06-11 06:24:41 +0530

    stage 8

--- app/main.py
+++ app/main.py
@@ -10,26 +10,27 @@ def main():
         sys.stdout.flush()
         command = input()
         cmd = command.split(" ",1)[0]
-        if cmd not in commands_list:
-            sys.stdout.write(f"{command}: not found\n")
+        paths = os.environ.get('PATH')
+        pa=None
+        if cmd == "exit":
+            sys.exit(0)
+        elif cmd == "echo":
+            sys.stdout.write(command.split(" ",1)[1] + "\n")
+        elif cmd == "type":
+            directories = paths.split(':')
+            for dir in directories:
+                if os.path.isfile(f"{dir}/{command.split(" ",1)[1]}") and os.access(f"{dir}/{command.split(" ",1)[1]}", os.X_OK):
+                    pa = f"{dir}/{command.split(" ",1)[1]}"
+            if command.split(" ",1)[1] in commands_list:
+                sys.stdout.write(f"{command.split(" ",1)[1]} is a shell builtin\n")
+            elif pa:
+                sys.stdout.write(f"{command.split(" ",1)[1]} is {pa}\n")
+            else:
+                sys.stdout.write(f"{command.split(' ',1)[1]}: not found\n")
+        elif os.path.isfile(f"{dir}/{command.split(" ",1)[0]}") and os.access(f"{dir}/{command.split(" ",1)[0]}", os.X_OK):
+            os.system(command)
         else:
-            if cmd == "exit":
-                sys.exit(0)
-            elif cmd == "echo":
-                sys.stdout.write(command.split(" ",1)[1] + "\n")
-            elif cmd == "type":
-                paths = os.environ.get('PATH')
-                pa=None
-                directories = paths.split(':')
-                for dir in directories:
-                    if os.path.isfile(f"{dir}/{command.split(" ",1)[1]}"):
-                        pa = f"{dir}/{command.split(" ",1)[1]}"
-                if command.split(" ",1)[1] in commands_list:
-                    sys.stdout.write(f"{command.split(" ",1)[1]} is a shell builtin\n")
-                elif pa:
-                    sys.stdout.write(f"{command.split(" ",1)[1]} is {pa}\n")
-                else:
-                    sys.stdout.write(f"{command.split(' ',1)[1]}: not found\n")
+            sys.stdout.write(f"{command.split(' ',1)[1]}: not found\n")
         sys.stdout.flush()
         
 
